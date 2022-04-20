eac6e6c85852b6c1a99db75ca6201467214d533d
Yuzi <2205816944@qq.com>
2022-04-20 21:21:27 +0800

    first commit

---
+++ EX1/index.js
@@ -0,0 +1,22 @@
+$(() => {
+    let menulevel1 = ["时间长河", "长廊一角", "人物掠影", "关于我们"];
+    for (const key in menulevel1) {
+        if (Object.hasOwnProperty.call(menulevel1, key)) {
+            const element = menulevel1[key];
+            $("header .menu").append($("<div class=\"menu" + key + "\"></div>").append("<span class=\"menulevel1\">" + element + "</span>"));
+        }
+    }
+    let menulevel2 = [["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"]];
+    for (const key in menulevel2) {
+        if (Object.hasOwnProperty.call(menulevel2, key)) {
+            const element = menulevel2[key];
+            $("header .menu .menu" + key).append("<ul></ul>");
+            $("header .menu .menu" + key).mouseenter(function () { $(this).find("ul").stop().slideDown(1000); });//绑定事件，鼠标悬停展开
+            $("header .menu .menu" + key).mouseleave(function () { $(this).find("ul").stop().slideUp(1000); });//鼠标离开升起
+            for (let j = 0; j < element.length; j++) {
+                $("header .menu .menu" + key + " ul").append("<li><a href=\"javascript:void(0)\">" + element[j] + "</a></li>");
+            }
+        }
+    }
+
+});
