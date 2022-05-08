28e342b157d767adffb0f4ae9b880b0b33712f6a
Yuzi0201 <2205816944@qq.com>
2022-05-08 23:42:49 +0800

    add img,footer

--- EX1/index.js
+++ EX1/index.js
@@ -6,15 +6,15 @@ $(() => {
             $("header .menu").append($("<div class=\"menu" + key + "\"></div>").append("<span class=\"menulevel1\">" + element + "</span>"));
         }
     }
-    let menulevel2 = [["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"]];
+    let menulevel2 = [["黎明前夜", "燎原星火", "曲折前行", "谱写新章"], ["占位符", "占位符", "占位符"], ["占位符", "占位符", "占位符"], []];
     for (const key in menulevel2) {
         if (Object.hasOwnProperty.call(menulevel2, key)) {
             const element = menulevel2[key];
             $("header .menu .menu" + key).append("<ul></ul>");
-            $("header .menu .menu" + key).mouseenter(function () { $(this).find("ul").stop().slideDown(1000); });//绑定事件，鼠标悬停展开
-            $("header .menu .menu" + key).mouseleave(function () { $(this).find("ul").stop().slideUp(1000); });//鼠标离开升起
+            $("header .menu .menu" + key).mouseenter(function () { $(this).find("ul").stop().slideDown(500); });//绑定事件，鼠标悬停展开
+            $("header .menu .menu" + key).mouseleave(function () { $(this).find("ul").stop().slideUp(500); });//鼠标离开升起
             for (let j = 0; j < element.length; j++) {
-                $("header .menu .menu" + key + " ul").append("<li><a href=\"javascript:void(0)\">" + element[j] + "</a></li>");
+                $("header .menu .menu" + key + " ul").append("<li>" + element[j] + "</li>");
             }
         }
     }
@@ -34,4 +34,5 @@ $(() => {
             pauseOnMouseEnter: true,
         },
     });
+
 });
